7a4ad3816bda8f01f6c1f130592a1606bf7f69d9
Terence Mbu <mbu.terence@gmail.com>
2023-05-14 11:14:49 +0200

    first commit

---
+++ components/Home.js
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div class=''>
+      <main class=''>
+        <h1 class=''>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
